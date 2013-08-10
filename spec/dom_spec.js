fbf63009d035425985cdc2dc2595f382c4677da0
Keisuke KITA <kei.kita2501@gmail.com>
2013-08-10 22:56:03 +0900

    試しに追加したのを除く

--- spec/dom_spec.js
+++ spec/dom_spec.js
@@ -2,7 +2,4 @@ describe('dom', function() {
   it('should be div', function() {
     expect($('<div id="some-id"></div>')).toBe('div');
   });
-  it('should be p', function() {
-    expect($('<p></p>')).toBe('p');
-  });
 });
