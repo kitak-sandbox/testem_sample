f1f93cecee08b039bc0b52bafa87850dbe4dae92
Keisuke KITA <kei.kita2501@gmail.com>
2013-08-10 22:36:26 +0900

    fix

--- spec/dom_spec.js
+++ spec/dom_spec.js
@@ -1,6 +1,8 @@
 describe('dom', function() {
   it('should be div', function() {
     expect($('<div id="some-id"></div>')).toBe('div');
+  });
+  it('should be p', function() {
     expect($('<p></p>')).toBe('p');
   });
 });
